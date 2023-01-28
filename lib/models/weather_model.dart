432c767bbaf3c70d3db098cfb80c1938ad2881be
Mohamed Ahmed <115259098+MOhamedAHmed1945@users.noreply.github.com>
2023-01-28 20:33:04 +0200

    create

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -19,7 +19,7 @@ class WeatherModel{
   date =  data['location']['localtime'];
   temp =  jsonData['avgtemp_c'];
   maxTemp =  jsonData['maxtemp_c'];
-  minTemp =  jsonData['mintemp_c'];
+  minTemp =  jsonData['mintemp_c'];//////
   weatherStateName =  jsonData['condition']['text'];
  }
 }
